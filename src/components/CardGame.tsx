9b287961088f918574596b4b1eefaac94dbb7eb5
ali <7731.aa@gmail.com>
2023-08-09 23:46:38 +0430

    feat: add filter genre

--- src/components/CardGame.tsx
+++ src/components/CardGame.tsx
@@ -1,75 +1,101 @@
 import {
-	Badge,
-	Card,
-	CardBody,
-	CardFooter,
-	Divider,
-	Heading,
-	Image,
-	Stack,
-	Text,
+  Badge,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Image,
+  Skeleton,
+  Stack,
+  Text,
+  Box,
+  SkeletonCircle,
 } from "@chakra-ui/react";
 import gamesData from "../data/games-data";
 import { AiFillWindows } from "react-icons/ai";
 
 interface Props {
-	id: number;
-	name: string;
-	metacritic: number;
-	background_image: string;
-	platforms: {
-		platform: {
-			id: number;
-			name: string;
-		};
-	}[];
-	isLoading: boolean;
+  id: number;
+  name: string;
+  metacritic: number;
+  background_image: string;
+  platforms: {
+    platform: {
+      id: number;
+      name: string;
+    };
+  }[];
+  isLoading: boolean;
 }
 
 const CardGame = ({
-	id,
-	name,
-	metacritic,
-	background_image,
-	platforms,
-	isLoading
+  id,
+  name,
+  metacritic,
+  background_image,
+  platforms,
+  isLoading = true,
 }: Props) => {
-	// const { results: games } = gamesData;
-
-	console.log(platforms);
-
-	return (
-		<>			<Card maxW="sm">
-				<CardBody p={0} pb={4} borderRadius={5}>
-					<Image
-						src={background_image}
-						borderTopRadius={5}
-						h={200}
-						w={"100%"}
-					/>
-					<Stack mt="6" spacing="3">
-						<Stack ml={2} direction={"row"}>
-							{platforms.map((item) => {
-								if (item.platform.name === "PC") {
-									return (
-										<p key={item.platform.id}>
-											{item.platform.name === "PC" && (
-												<AiFillWindows />
-											)}
-										</p>
-									);
-								}
-							})}
-							<Badge colorScheme="green">{metacritic}</Badge>
-						</Stack>
-						<Heading size="md" p={2}>
-							{name}
-						</Heading>
-					</Stack>
-				</CardBody>
-			</Card>
-		</>
-	);
+  return (
+    <>
+      {isLoading && (
+        <Stack>
+          <Box display="flex" alignItems={"center"}>
+            <SkeletonCircle size="8" />
+            <Skeleton height="8px" marginLeft={4}>
+              <Box width={24} height={8} marginLeft={3} />
+            </Skeleton>
+          </Box>
+          <Box display="flex" alignItems={"center"}>
+            <SkeletonCircle size="8" />
+            <Skeleton height="8px" marginLeft={4}>
+              <Box width={24} height={8} marginLeft={3} />
+            </Skeleton>
+          </Box>
+          <Box display="flex" alignItems={"center"}>
+            <SkeletonCircle size="8" />
+            <Skeleton height="8px" marginLeft={4}>
+              <Box width={24} height={8} marginLeft={3} />
+            </Skeleton>
+          </Box>
+          <Box display="flex" alignItems={"center"}>
+            <SkeletonCircle size="8" />
+            <Skeleton height="8px" marginLeft={4}>
+              <Box width={24} height={8} marginLeft={3} />
+            </Skeleton>
+          </Box>
+        </Stack>
+      )}
+      <Card maxW="sm">
+        <CardBody p={0} pb={4} borderRadius={5}>
+          <Image
+            src={background_image}
+            borderTopRadius={5}
+            h={200}
+            w={"100%"}
+          />
+          <Stack mt="6" spacing="3">
+            <Stack ml={2} direction={"row"}>
+              {platforms.map((item) => {
+                if (item.platform.name === "PC") {
+                  return (
+                    <p key={item.platform.id}>
+                      {item.platform.name === "PC" && <AiFillWindows />}
+                    </p>
+                  );
+                }
+              })}
+              <Badge colorScheme="green">{metacritic}</Badge>
+            </Stack>
+            <Heading size="md" p={2}>
+              {name}
+            </Heading>
+          </Stack>
+        </CardBody>
+      </Card>
+    </>
+  );
 };
 
 export default CardGame;
--- src/components/MainGame.tsx
+++ src/components/MainGame.tsx
@@ -1,14 +1,14 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
-	Box,
-	Button,
-	Grid,
-	Heading,
-	Menu,
-	MenuButton,
-	MenuItem,
-	MenuList,
-	useColorMode,
+  Box,
+  Button,
+  Grid,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useColorMode,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import platformsData from "../data/platforms-data";
@@ -17,91 +17,107 @@ import CardGame from "./CardGame";
 import axios from "axios";
 
 interface Props {
-	selectedGenre: string;
+  selectedGenre: string;
 }
 
 interface Games {
-	id: number;
-	name: string;
-	background_image: string;
-	metacritic: number;
-	platforms: {
-		platform: {
-			id: number;
-			name: string;
-		};
-	}[];
+  id: number;
+  name: string;
+  background_image: string;
+  metacritic: number;
+  platforms: {
+    platform: {
+      id: number;
+      name: string;
+    };
+  }[];
+  genres: {
+    id: number;
+    name: string;
+  }[];
 }
 
 const MainGame = ({ selectedGenre }: Props) => {
-	const { results: platforms } = platformsData;
-	// const { results: games } = gamesData;
-	const { colorMode, setColorMode } = useColorMode();
-	const [games, setGames] = useState<Games[]>([]);
-	const [error, setError] = useState("");
-	const [isLoading, setLoading] = useState(false);
+  const { results: platforms } = platformsData;
+  // const { results: games } = gamesData;
+  const { colorMode, setColorMode } = useColorMode();
+  const [games, setGames] = useState<Games[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-	useEffect(() => {
-		setLoading(true);
-		axios
-			.get("http://127.0.0.1:8000/")
-			.then((res) => {
-				setGames(res.data.results);
-				setLoading(false);
-			})
-			.catch((err) => {
-				setError(err.message);
-				setLoading(false);
-			});
-	}, []);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://127.0.0.1:3000/")
+      .then((res) => {
+        console.log(res);
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
 
-	return (
-		<>
-			<Heading color={colorMode === "dark" ? "white" : "black"}>
-				{selectedGenre} Games
-			</Heading>
-			<Box display="flex" alignItems={"center"} paddingTop={8} gap={10}>
-				<Box>
-					<Menu>
-						<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-							Platforms
-						</MenuButton>
-						<MenuList>
-							{platforms.map((platform) => (
-								<MenuItem>{platform.name}</MenuItem>
-							))}
-						</MenuList>
-					</Menu>
-				</Box>
-				<Box>
-					<Menu>
-						<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-							Orderby: Relevance
-						</MenuButton>
-						<MenuList>
-							<MenuItem>Download</MenuItem>
-							<MenuItem>Create a Copy</MenuItem>
-							<MenuItem>Mark as Draft</MenuItem>
-							<MenuItem>Delete</MenuItem>
-							<MenuItem>Attend a Workshop</MenuItem>
-						</MenuList>
-					</Menu>
-				</Box>
-			</Box>
-			<Grid templateColumns={"repeat(3, 1fr)"} paddingTop={4} gap={4}>
-				{games.map((game) => (
-					<CardGame
-						id={game.id}
-						name={game.name}
-						background_image={game.background_image}
-						metacritic={game.metacritic}
-						platforms={game.platforms}
-						isLoading={isLoading}
-					/>
-				))}
-			</Grid>
-		</>
-	);
+  const filterByGenreName = (data: Games[], genreName: string) => {
+    return data.filter((game) => {
+      return game.genres.some((genre) => genre.name === genreName);
+    });
+  };
+
+  const visibleGames = selectedGenre
+    ? filterByGenreName(games, selectedGenre)
+    : games;
+
+  return (
+    <>
+      <Heading color={colorMode === "dark" ? "white" : "black"}>
+        {selectedGenre} Games
+      </Heading>
+      <Box display="flex" alignItems={"center"} paddingTop={8} gap={10}>
+        <Box>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Platforms
+            </MenuButton>
+            <MenuList>
+              {platforms.map((platform) => (
+                <MenuItem key={platform.id}>{platform.name}</MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+        </Box>
+        <Box>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Orderby: Relevance
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem>Delete</MenuItem>
+              <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Box>
+      <Grid templateColumns={"repeat(3, 1fr)"} paddingTop={4} gap={4}>
+        {visibleGames.map((game) => (
+          <CardGame
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            background_image={game.background_image}
+            metacritic={game.metacritic}
+            platforms={game.platforms}
+            isLoading={isLoading}
+          />
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default MainGame;
